869f574f4fd9f235f3fb30e2eaa12bfe8a5c22a2
shofiq0023 <shofiqulislam0023@gmail.com>
2025-06-22 22:40:22 +0600

    added delete dialog

---
+++ lib/widgets/dialogs/entry_category/entry_category_delete_confirmation_dialog.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:popcorn/models/entities/entry_category.dart';
+import 'package:popcorn/models/entities/watchlist_entry.dart';
+import 'package:popcorn/providers/entry_category_provider.dart';
+import 'package:popcorn/providers/watchlist_entry_provider.dart';
+import 'package:provider/provider.dart';
+
+class EntryCategoryDeleteConfirmationDialog extends StatefulWidget {
+  final EntryCategory entryCategory;
+  const EntryCategoryDeleteConfirmationDialog({super.key, required this.entryCategory});
+
+  @override
+  State<EntryCategoryDeleteConfirmationDialog> createState() => _EntryCategoryDeleteConfirmationDialogState();
+}
+
+class _EntryCategoryDeleteConfirmationDialogState extends State<EntryCategoryDeleteConfirmationDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Are you sure?'),
+      content: SizedBox(
+        width: 600,
+        child: const Text("This will permanently delete the category!"),
+      ),
+      actions: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            /// Close button
+            MaterialButton(
+              child: const Text("NO"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+
+            /// Confirmation Button
+            MaterialButton(
+              child: const Text("YES", style: TextStyle(color: Colors.red)),
+              onPressed: () {
+                Provider.of<EntryCategoryProvider>(
+                  context,
+                  listen: false,
+                ).delete(widget.entryCategory);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- lib/widgets/entry_category_widget.dart
+++ lib/widgets/entry_category_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:popcorn/models/entities/entry_category.dart';
+import 'package:popcorn/widgets/dialogs/entry_category/entry_category_delete_confirmation_dialog.dart';
 
 class EntryCategoryItemWidget extends StatefulWidget {
   final EntryCategory entryCategory;
@@ -16,9 +17,38 @@ class _EntryCategoryItemWidgetState extends State<EntryCategoryItemWidget> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {},
-      child: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Text(widget.entryCategory.categoryName),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(5.0),
+          boxShadow: const <BoxShadow>[
+            BoxShadow(
+              color: Color.fromRGBO(0, 0, 0, 0.2),
+              blurRadius: 3,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(widget.entryCategory.categoryName),
+            IconButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder:
+                      (context) => EntryCategoryDeleteConfirmationDialog(
+                        entryCategory: widget.entryCategory,
+                      ),
+                );
+              },
+              icon: Icon(Icons.delete_forever, color: Colors.red),
+            ),
+          ],
+        ),
       ),
     );
   }
